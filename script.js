f30eb2ca4c68f86454b165df93e07b7372fd35a2
yusramunirahmed111 <yusramunirahmed1@gmail.com>
2024-12-16 15:38:50 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let body = document.getElementById("form")
+function myfunc(){
+    let answer = document.getElementById("name").value;
+     let ans = document.getElementById("email").value;
+     let an = document.getElementById("pass").value;
+     console.log( "name", answer, "email",ans ,"password", an, );
+}
